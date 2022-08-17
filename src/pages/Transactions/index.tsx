810c301c06279d5d2374ee3487c093298acdba6b
Gabriel Menezes <gabrielsmenezes99@gmail.com>
2022-08-17 19:54:53 -0300

    chore(Component): Summary

--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -1,9 +1,12 @@
 import { Header } from "../../components/Header";
+import { Summary } from "../../components/Summary";
 
 export function TransactionsPage() {
   return (
     <>
       <Header />
+
+      <Summary />
     </>
   );
 }
